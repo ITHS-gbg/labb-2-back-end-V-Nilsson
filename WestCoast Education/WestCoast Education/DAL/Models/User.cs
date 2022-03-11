713e26626b29d93aad5988e121c83bcab054b446
V-Nilsson <89032509+V-Nilsson@users.noreply.github.com>
2022-03-11 11:34:23 +0100

    Added base for CRUD

--- WestCoast Education/WestCoast Education/DAL/Models/User.cs
+++ WestCoast Education/WestCoast Education/DAL/Models/User.cs
@@ -2,5 +2,15 @@
 {
     public class User
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+        public string Cellphone { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+
     }
 }
---
+++ WestCoast Education/WestCoast Education/DAL/WCEStorage.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+
+namespace WestCoast_Education.DAL.Models
+{
+    public class WCEStorage
+    {
+        private readonly IDictionary<int, Course> _courses;
+        private int _id;
+
+        public WCEStorage()
+        {
+            _courses = new Dictionary<int, Course>();
+        }
+        //TODO Create CRUD operations for Courses
+
+        public bool CreateCourse(Course course)
+        {
+            if (_courses.Values.Contains(course))
+            {
+                return false;
+            }
+            _courses.Add(_id++, course);
+            return true;
+        }
+
+        public ICollection<Course> GetAllCourses()
+        {
+            return _courses.Values;
+        }
+
+        public Course GetCourse(int id)
+        {
+            if (!_courses.Keys.Contains(id))
+            {
+                return null;
+            }
+            return _courses[id];
+        }
+
+        public bool UpdateCourse(int id, Course course)
+        {
+            if (!_courses.Keys.Contains(id))
+            {
+                return false;
+            }
+
+            _courses[id] = course;
+            return true;
+        }
+
+        public bool DeleteCourse(int id)
+        {
+            if (!_courses.Keys.Contains(id))
+            {
+                return false;
+            }
+
+            _courses.Remove(id);
+            return true;
+        }
+    }
+}
